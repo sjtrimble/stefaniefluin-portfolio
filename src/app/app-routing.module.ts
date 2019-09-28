15018aad3a15f4d44d863fc1dd5960151aff420d
Stefanie Fluin <sjtrimble@gmail.com>
2019-09-28 13:46:47 -0700

    add components and scss structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { AppComponent } from "./app.component";
+import { AboutComponent } from "./about/about.component";
+import { WorkComponent } from './work/work.component';
+import { HomeComponent } from './home/home.component';
+import { ExplorationsComponent } from './explorations/explorations.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "about",
+    component: AboutComponent
+  },
+  {
+    path: "work",
+    component: WorkComponent
+  },
+  {
+    path: "explorations",
+    component: ExplorationsComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { WorkComponent } from './work/work.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { ExplorationsComponent } from './explorations/explorations.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HomeComponent,
+    WorkComponent,
+    NavbarComponent,
+    FooterComponent,
+    ExplorationsComponent
   ],
   imports: [
     BrowserModule,
