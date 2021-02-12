7f58139bb70b0133cb8ac5f2317eca059a988358
Stefanie Fluin <sjtrimble@gmail.com>
2021-02-12 15:41:28 -0800

    Make navbar sticky and add cond scroll styles

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,14 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  templateUrl: './navbar.component.html'
+  templateUrl: './navbar.component.html',
 })
 export class NavbarComponent implements OnInit {
+  styleHeader = false;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollLocation =
+      window.pageYOffset ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop ||
+      0;
+    if (scrollLocation > 100) {
+      this.styleHeader = true;
+    }
+    if (scrollLocation < 100) {
+      this.styleHeader = false;
+    }
   }
-
 }
